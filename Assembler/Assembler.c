90f6f1b1b3bfbd15781d90be8a3b681d826935c7
Lior20 <liorbi220@gmail.com>
2022-12-03 12:23:14 +0200

    Start working on Assembler.c

--- Assembler/Assembler.c
+++ Assembler/Assembler.c
@@ -1,1 +1,33 @@
-/.
+# include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_LINE 300 // The maximum line size of the input file is 300.
+#define MAX_LABEL 50 // the maximum lable size is 50.
+
+typedef struct 
+{
+	int opcode;
+	int rd;
+	int rs;
+	int rt;
+}R_Format;
+
+typedef struct
+{
+	int opcode;
+	int rd;
+	int rs;
+	int rt;
+	int imm;
+}J_Format;
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+	
+}
