06cc7c9e7d50f6d5d194d304611d65f2eefee567
KerenDruker <kedr97@gmail.com>
2022-12-13 20:02:47 +0200

    RegArry

--- Assembler/Assembler.c
+++ Assembler/Assembler.c
@@ -6,6 +6,8 @@
 #define MAX_LINE 300 // The maximum line size of the input file is 300.
 #define MAX_LABEL 50 // the maximum lable size is 50.
 #define MAX_MEMO_LINE 4096
+#define NUM_OF_REG 16
+int RegArry[NUM_OF_REG]
 
 enum OPCODE{
 		add, sub, mul, and, or, xor, sll, sra, srl, beq, bne, blt, bgt, ble, bge, jal, lw, sw, reti, in, out, halt,
@@ -35,7 +37,7 @@ typedef struct
 
 typedef struct
 {
-	int op; // R_FORMAT, I_FORMAT, J_FORMAT aaaaaa
+	int op; // R_FORMAT, I_FORMAT, J_FORMAT 
 	int rs;
 	int rt;
 	int rd;
@@ -123,9 +125,94 @@ int TranslateOpcode(char *op) {
 	return halt;
 }
 
+int GetRegister (char *reg)
+{
+	if (strcmp(reg, "$zero") == 0) {
+		return $zero;
+	}
+	if (strcmp(reg, "imm") == 0) {
+		return $imm;
+	}
+	if (strcmp(reg, "$v0") == 0) {
+		return $v0;
+	}
+	if (strcmp(reg, "$a0") == 0) {
+		return $a0;
+	}
+	if (strcmp(reg, "$a1") == 0) {
+		return $a1;
+	}
+	if (strcmp(reg, "$a3") == 0) {
+		return $a3;
+	}
+	if (strcmp(reg, "$t0") == 0) {
+		return $t0;
+	}
+	if (strcmp(reg, "$t1") == 0) {
+		return $t1;
+	}
+	if (strcmp(reg, "$t2") == 0) {
+		return $t2;
+	}
+	if (strcmp(reg, "$s0") == 0) {
+		return $s0;
+	}
+	if (strcmp(reg, "$s1") == 0) {
+		return $s1;
+	}
+	if (strcmp(reg, "$s2") == 0) {
+		return $s2;
+	}
+	if (strcmp(reg, "$gp") == 0) {
+		return $gp;
+	}
+	if (strcmp(reg, "$sp") == 0) {
+		return $sp;
+	}
+	if (strcmp(reg, "$ra") == 0) {
+		return $ra;
+	}
+}
 
+void Do_R_inst(int op, int rd int rs, int rt )
+{
+	switch (op)
+	{
+	case add:
+		RegArry[rd] = RegArry[rs] + RegArry[rt];
+		break;
+	case sub:
+		RegArry[rd] = RegArry[rs] - RegArry[rt];
+		break;
+	case mul:
+		RegArry[rd] = RegArry[rs] * RegArry[rt];
+		break;
+	case and:
+
+
+	default:
+		// default statements
+	}
+}
 
 int main(int argc, char* argv[])
 {
-	
+	FILE *fp;
+	int PC = 0;
+	FILE *StartFile;
+	fp = fopen("fileName","r");
+	if (fp == NULL)
+	{
+		printf("Error: opening file failed\n");
+		return 1;
+	}
+
+	startFile = fp
+
+	do
+	{
+
+	};
+	while (!feof(fp));
+	fclose(fp);
 }
