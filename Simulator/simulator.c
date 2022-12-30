6040a0cba8ccbfeb6f30c857b71afc99464f92e3
Lior Ben Ishay <103303629+Lior20@users.noreply.github.com>
2022-12-30 15:44:14 +0200

    Create simulator.c

---
+++ Simulator/simulator.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_LINE 300 // The maximum line size of the input file is 300.
+#define MAX_LABEL 50 // the maximum lable size is 50.
+#define MAX_MEMO_LINE 4096
