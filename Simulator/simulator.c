d62d10b3d1eaa20c699aa3f3cd25b1bcc56afdc2
Lior20 <liorbi220@gmail.com>
2022-12-30 15:54:01 +0200

    initialize Simulator.c

--- Simulator/simulator.c
+++ Simulator/simulator.c
@@ -6,3 +6,10 @@
 #define MAX_LINE 300 // The maximum line size of the input file is 300.
 #define MAX_LABEL 50 // the maximum lable size is 50.
 #define MAX_MEMO_LINE 4096
+
+
+int PC = 0;
+
+int main(int argc, char* argv[]) {
+
+}
